2a1494a5f58c1af7e49d4737d40f527b80c0f32a
veq03401 <veq03401@gmail.com>
2016-11-13 14:12:51 +0900

    master3コミット1

--- src/main/java/com/sample/dao/UserDao.java
+++ src/main/java/com/sample/dao/UserDao.java
@@ -8,6 +8,8 @@ import com.sample.business.model.User;
 //master2コメント2
 //master2コメント3
 //master1コメント
+//master3コメント1
+//master3コメント2
 public class UserDao {
 	 public User getUser(String id){
 	   //ここではハードコードしていますが、本当はDBから値を取得します。
